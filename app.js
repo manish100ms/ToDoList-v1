c5e82d251953c00ae007f1b116f02405a04ad6c8
Manish Sharma <manish100ms@gmail.com>
2024-01-07 23:09:06 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const homeItems = [];
+const workItems = [];
+
+app.get("/", function (req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("index", {
+    listTitle: day,
+    itemsArr: homeItems,
+  });
+});
+
+app.get("/work", function (req, res) {
+  res.render("index", {
+    listTitle: "Work List",
+    itemsArr: workItems,
+  });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  // Prevent empty items
+  if (item !== "") {
+    if (req.body.itemType === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      homeItems.push(item);
+      res.redirect("/");
+    }
+  }
+});
+
+app.listen("3000", function () {
+  console.log("Server listening on port 3000");
+});
